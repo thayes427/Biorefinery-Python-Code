302bcfe34025c889b53b91d79a04d6ede6de8e61
hayest1 <hayest1@us.ibm.com>
2018-10-28 12:36:34 -0500

    comment

--- GUI.py
+++ GUI.py
@@ -6,6 +6,8 @@ from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 #reload(mod)
 
+
+## COMMENT TEST
 ################Global Constants###############
 OIL_TO_MA = (1.378,648.5,3)
 MA_TO_SA = (.7465,1050,3)
