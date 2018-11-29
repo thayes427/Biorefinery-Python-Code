1b452f29b2045c68abc427da88534acb9ff7ade4
aadi90 <arawal97@uchicago.edu>
2018-11-29 16:13:03 -0600

    merging code

--- obj2_cleaned_up_box.py
+++ obj2_cleaned_up_box.py
@@ -224,8 +224,7 @@ def multivariate_sensitivity_analysis(aspenfilename, excelfilename,
         excel.Run('SOLVE_DCFROR')
         
         dfstreams.loc[trial] = case_values + [x.Value for x in book.Sheets('Output').Evaluate("C3:C15")]
-        if graph_plot == 1:
-            GUI.plot_on_GUI(dfstreams, vars_to_change)
+        GUI.plot_on_GUI(dfstreams, vars_to_change)
         
         ######### KEEP TRACK OF RUN TIME PER TRIAL ########
         print('Elapsed Time: ', time() - old_time)
